754d446b56a5b5647b4bc930f7655c15c6b18cb5
frajola <ggmoraes@protonmail.com>
2025-03-08 22:07:18 -0300

    render settings update

--- conselho/settings.py
+++ conselho/settings.py
@@ -120,15 +120,15 @@ import os
 STATIC_URL = "/static/"
 
 # Diretório onde os arquivos estáticos serão coletados ao rodar "collectstatic"
-STATIC_ROOT = os.path.join(BASE_DIR, "main/staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "main/media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Adiciona os diretórios estáticos do app (caso necessário)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "main/static"),
+    os.path.join(BASE_DIR, "main", "static"),
 ]
 
 
